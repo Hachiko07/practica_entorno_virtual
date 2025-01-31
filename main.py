1c50310839d1a229cc526eb9f975da2dc55f72e3
Miros González <miros261203@gmail.com>
2025-01-31 02:29:53 -0600

    Primer commit

---
+++ main.py
@@ -0,0 +1,10 @@
+﻿# -*- coding: utf-8 -*-
+
+import requests
+
+response = requests.get("https://api.github.com")
+if response.status_code == 200:
+    print("¡Conexión exitosa a la API de GitHub!")
+else:
+    print("Algo salió mal:", response.status_code)
+
